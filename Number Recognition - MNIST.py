a9cb143e539db88be3cd7a0c9318ec1164fb4bd8
Pradeep Selvaraj <pradeep.selvaraj.23@gmail.com>
2022-05-27 19:08:25 +0100

    Update

--- Number Recognition - MNIST.py
+++ Number Recognition - MNIST.py
@@ -113,14 +113,13 @@ print(fit(10 , 0.001, model, train_dl, val_dl))
 
 # # TEST PHASE
 
-# def predict_image(img, model):
-#     xb = img.unsqueeze(0)
-#     yb = model(xb)
-#     _, preds = torch.max(yb, dim=1)
-#     return preds[0].item()
+def predict_image(img, model):
+    xb = img.unsqueeze(0)
+    yb = model(xb)
+    _, preds = torch.max(yb, dim=1)
+    return preds[0].item()
 
 
-# img, label = test_dataset[0]
-# plt.imshow(img[0], cmap='gray')
-# print('Label:', label, ', Predicted:', predict_image(img, model))
-
+img, label = test_dataset[0]
+plt.imshow(img[0], cmap='gray')
+print('Label:', label, ', Predicted:', predict_image(img, model))
