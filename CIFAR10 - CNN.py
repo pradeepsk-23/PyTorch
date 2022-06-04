154301d64b06728c73109b94acbe70aeceeb8387
Pradeep Selvaraj <pradeep.selvaraj.23@gmail.com>
2022-06-04 11:05:12 +0100

    Updated

--- CIFAR10 - CNN.py
+++ CIFAR10 - CNN.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torchvision
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 
@@ -20,25 +21,34 @@ train_dl = DataLoader(train_dataset, batch_size, shuffle=True)
 test_dl = DataLoader(test_dataset, batch_size)
 
 # Fully connected neural network with one hidden layer
-input_size = 3*32*32
-hidden_size = 1536
 output_size = 10
 
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_size, hidden_size)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(hidden_size, output_size)
+class ConvNet(nn.Module):
+    def __init__(self, output_size):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=5, stride=1, padding=2),
+            # Output pixel = {[Input Size(32 in this case) - K(Kernel) + 2P(Padding)] / S(Stride)} + 1. Default Stride=1, Padding=0.
+            # out = [64, 32, 32]
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)) # out = [64, 16, 16]
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=5, stride=1, padding=2), # out = [128, 16, 16]
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)) # out = [128, 8, 8]
+        self.fc = nn.Linear(8*8*128, output_size)
     
     def forward(self, x):
-        out = self.fc1(x)
-        out = self.relu(out)
-        out = self.fc2(out)
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
         return out
 
 # Model
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model = ConvNet(output_size).to(device)
 
 # Loss and optimizer
 # F.cross_entropy computes softmax internally
@@ -50,9 +60,8 @@ epochs = 10
 total_step = len(train_dl)
 for epoch in range(epochs):
     for i, (images, labels) in enumerate(train_dl):
-        # Reshape images to (batch_size, input_size)  
         # Move tensors to the configured device
-        images = images.reshape(-1, input_size).to(device)
+        images = images.to(device)
         labels = labels.to(device)
 
         # Forward pass
@@ -74,7 +83,7 @@ with torch.no_grad():
     correct = 0
     total = 0
     for images, labels in test_dl:
-        images = images.reshape(-1, input_size).to(device)
+        images = images.to(device)
         labels = labels.to(device)
         outputs = model(images)
         _, predicted = torch.max(outputs.data, 1)
